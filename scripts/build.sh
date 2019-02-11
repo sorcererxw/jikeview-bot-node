80094f0e0f3e833b6e67fe8905955a57ca26569d
SorcererXW <tujipqy@gmail.com>
2019-02-11 12:48:28 +0800

    integrate circle ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 
 chmod +x ./gradlew
 export image_version=$(./gradlew :app:getVersion -q)
-echo ${image_version}
+echo "version: ${image_version}"
 ./gradlew dockerBuildImage
 docker images
-docker tag sorcererxw/jikeview-bot:${image_version} sorcererxw/jikeview-bot:latest
-docker push sorcererxw/jikeview-bot:${image_version}
+eval "docker tag sorcererxw/jikeview-bot:${image_version} sorcererxw/jikeview-bot:latest"
+eval "docker push sorcererxw/jikeview-bot:${image_version}"
 docker push sorcererxw/jikeview-bot:latest
